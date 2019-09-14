26e52a778d5a3f7b9090f837d05ec4418074cd4e
Colton <colton_rusch@brown.edu>
2019-09-14 16:00:40 -0400

    Browser name

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.root}>
         <View style={styles.header}>
-          <Text style={styles.headerText}>Embedded browser!</Text>
+          <Text style={styles.headerText}>SafeSpace</Text>
         </View>
 
         <View style={styles.browser}>
